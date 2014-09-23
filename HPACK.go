11e038502ddca3903eaf0742ca4527f139ebe9f5
Daiki Aminaka <1991.daiki@gmail.com>
2014-09-23 23:17:36 +0900

    parse naive literal

--- HPACK.go
+++ HPACK.go
@@ -9,25 +9,58 @@ type Header struct {
 	Name, Value string
 }
 
-func ParseIntRepresentation(buf []byte, N byte) (index byte, c int) {
-	I := (buf[0] & ((1 << N) - 1))
-	cursor := 1
+func ParseIntRepresentation(buf []byte, N byte) (I int, cursor byte) {
+	I = int(buf[0] & ((1 << N) - 1)) // byte could be used as byte
+	cursor = 1
 	if I < ((1 << N) - 1) {
 		return I, cursor
 	} else {
 		var M byte = 0
 		for (buf[cursor] & 0x80) > 0 {
-			I += (buf[cursor] & 0x7f) * (1 << M)
+			I += int(buf[cursor]&0x7f) * (1 << M)
 			M += 7
 			cursor += 1
 		}
-		I += (buf[cursor] & 0x7f) * (1 << M)
+		I += int(buf[cursor]&0x7f) * (1 << M)
 		return I, cursor + 1
 	}
 }
 
-func ParseHeader() {
+func ExtractContent(buf []byte, length int, isHuffman bool) (content string) {
+	if isHuffman {
+		return content
+	} else {
+		content = string(buf[:length])
+		return content
+	}
+}
 
+func ParseFromByte(buf []byte) (content string, cursor byte) {
+	isHuffman := false
+	if buf[0]&0x80 > 0 {
+		isHuffman = true
+	}
+	length, cursor := ParseIntRepresentation(buf, 7)
+	content = ExtractContent(buf[cursor:], length, isHuffman)
+	cursor += byte(length)
+	return content, cursor
+}
+
+func ParseHeader(index int, table int, buf []byte, isIndexed bool) (name, value string, cursor byte) {
+	if c := byte(0); !isIndexed {
+		if index == 0 {
+			name, c = ParseFromByte(buf[cursor:])
+			cursor += c
+		}
+		value, c = ParseFromByte(buf[cursor:])
+		cursor += c
+	}
+
+	if index > 0 {
+		//get header from table
+	}
+
+	return name, value, cursor
 }
 
 func decode(wire string) []Header {
@@ -39,36 +72,37 @@ func decode(wire string) []Header {
 	}
 	buf = &nums
 
-	//for i, v := range *buf {
-	//	fmt.Println(i, v)
-	//}
-
-	cursor := 0
-	for cursor < len(nums) {
+	var cursor byte = 0
+	for cursor < byte(len(nums)) {
 		isIndexed := false
 		isIncremental := false
-		var index byte
+		var index int
+		var c byte
 		if (*buf)[cursor]&0xe0 == 0x20 {
-			//change table size
+			// 7.3 Header Table Size Update
+			cursor = 1
 		} else if ((*buf)[cursor] & 0x80) > 0 {
+			// 7.1 Indexed Header Field
 			if ((*buf)[cursor] & 0x7f) == 0 {
 				panic('a')
 			}
-			index, c := ParseIntRepresentation((*buf)[cursor:], 7)
-			cursor += c
+			index, c = ParseIntRepresentation((*buf)[cursor:], 7)
 			isIndexed = true
 		} else {
-			c := 0
 			if (*buf)[cursor]&0xc0 == 0x40 {
-				index, c := ParseIntRepresentation((*buf)[cursor:], 6)
+				// 7.2.1 Literal Header Field with Incremental Indexing
+				index, c = ParseIntRepresentation((*buf)[cursor:], 6)
 				isIncremental = true
 			} else if (*buf)[cursor]&0xf0 == 0xf0 {
-				index, c := ParseIntRepresentation((*buf)[cursor:], 4)
+				// 7.2.3 Literal Header Field never Indexed
+				index, c = ParseIntRepresentation((*buf)[cursor:], 4)
 			} else {
-				index, c := ParseIntRepresentation((*buf)[cursor:], 4)
+				// 7.2.2 Literal Header Field without Indexing
+				index, c = ParseIntRepresentation((*buf)[cursor:], 4)
 			}
-			cursor += c
 		}
+		cursor += c
+
 		table := 1 //for test
 		name, value, c := ParseHeader(index, table, (*buf)[cursor:], isIndexed)
 		cursor += c
@@ -76,7 +110,7 @@ func decode(wire string) []Header {
 		if isIncremental {
 			//add to table
 		}
-		Headers.append(Header{name, value})
+		Headers = append(Headers, []Header{{name, value}}...)
 	}
 
 	//d := hex.EncodeToString(nums)
@@ -85,6 +119,10 @@ func decode(wire string) []Header {
 }
 
 func main() {
-	decode("ff80000111")
+	//nums, _ := hex.DecodeString(string("1FA18DB701"))
+	//fmt.Println(nums)
+	//fmt.Println(ParseIntRepresentation(nums, 5))
+	//decode("ff80000111")
+	fmt.Println(decode("00073a6d6574686f640347455400073a736368656d650468747470000a3a617574686f726974790f7777772e7961686f6f2e636f2e6a7000053a70617468012f"))
 	fmt.Println()
 }
