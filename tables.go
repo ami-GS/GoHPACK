319db4f07a562abaa98879a993661a13d4e82429
Daiki Aminaka <1991.daiki@gmail.com>
2014-09-27 23:56:12 +0900

    added static table (temporaly)

---
+++ tables.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Header struct {
+	Name, Value string
+}
+
+var STATIC_TABLE *[]Header = &[]Header{
+	{"", ""},
+	{":authority", ""},
+	{":method", "GET"},
+	{":method", "POST"},
+	{":path", "/"},
+	{":path", "/index.html"},
+	{":scheme", "http"},
+	{":scheme", "https"},
+	{":status", "200"},
+	{":status", "204"},
+	{":status", "206"},
+	{":status", "304"},
+	{":status", "400"},
+	{":status", "404"},
+	{":status", "500"},
+	{"accept-charset", ""},
+	{"accept-encoding", "gzip, deflate"},
+	{"accept-language", ""},
+	{"accept-ranges", ""},
+	{"accept", ""},
+	{"access-control-allow-origin", ""},
+	{"age", ""},
+	{"allow", ""},
+	{"authorization", ""},
+	{"cache-control", ""},
+	{"content-disposition", ""},
+	{"content-encoding", ""},
+	{"content-language", ""},
+	{"content-length", ""},
+	{"content-location", ""},
+	{"content-range", ""},
+	{"content-type", ""},
+	{"cookie", ""},
+	{"date", ""},
+	{"etag", ""},
+	{"expect", ""},
+	{"expires", ""},
+	{"from", ""},
+	{"host", ""},
+	{"if-match", ""},
+	{"if-modified-since", ""},
+	{"if-none-match", ""},
+	{"if-range", ""},
+	{"if-unmodified-since", ""},
+	{"last-modified", ""},
+	{"link", ""},
+	{"location", ""},
+	{"max-forwards", ""},
+	{"proxy-authenticate", ""},
+	{"proxy-authorization", ""},
+	{"range", ""},
+	{"referer", ""},
+	{"refresh", ""},
+	{"retry-after", ""},
+	{"server", ""},
+	{"set-cookie", ""},
+	{"strict-transport-security", ""},
+	{"transfer-encoding", ""},
+	{"user-agent", ""},
+	{"vary", ""},
+	{"via", ""},
+	{"www-authenticate", ""},
+}
+var STATIC_TABLE_NUM int = len(*STATIC_TABLE)
+
+//var NAME_TABLE = [header[0} for header in STATIC_TABLE]
+
+func main() {
+	fmt.Println(STATIC_TABLE)
+}
