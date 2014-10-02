68d2f56970cb38d6f048b62b571820e8df298b73
Daiki Aminaka <1991.daiki@gmail.com>
2014-10-02 21:44:12 +0900

    implement Encoder test

--- test.go
+++ test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"hpack"
@@ -24,14 +25,18 @@ type Case struct {
 
 var TESTCASE = []string{
 	"hpack-test-case/haskell-http2-naive/",
-	"hpack-test-case/haskell-http2-naive-huffman/",
-	"hpack-test-case/haskell-http2-static/",
-	"hpack-test-case/haskell-http2-static-huffman/",
-	"hpack-test-case/haskell-http2-linear/",
-	"hpack-test-case/haskell-http2-linear-huffman/",
+	//"hpack-test-case/haskell-http2-naive-huffman/",
+	//"hpack-test-case/haskell-http2-static/",
+	//"hpack-test-case/haskell-http2-static-huffman/",
+	//"hpack-test-case/haskell-http2-linear/",
+	//"hpack-test-case/haskell-http2-linear-huffman/",
 }
 
 func main() {
+	//fmt.Println(hex.EncodeToString(*hpack.PackIntRepresentation(3000000, 5)))
+	//nums, _ := hex.DecodeString(string("1fa18db701"))
+	//fmt.Println(hpack.ParseIntRepresentation(nums, 5))
+	args := os.Args
 	huffman.Root.CreateTree()
 	for _, testCase := range TESTCASE {
 		files, err := ioutil.ReadDir(testCase)
@@ -46,26 +51,51 @@ func main() {
 			var jsontype jsonobject
 			json.Unmarshal(data, &jsontype)
 			storyPass := true
-			for _, seq := range jsontype.Cases {
-				//fmt.Printf("%d\n", len(seq.Wire))
-				Headers := hpack.Decode(seq.Wire)
-				testHeaders := []hpack.Header{}
-				for _, dict := range seq.Headers {
-					for k, v := range dict {
-						testHeaders = append(testHeaders, hpack.Header{k, v})
+			if len(args) == 1 || args[1] == "-d" {
+				for _, seq := range jsontype.Cases {
+					Headers := hpack.Decode(seq.Wire)
+					testHeaders := []hpack.Header{}
+					for _, dict := range seq.Headers {
+						for k, v := range dict {
+							testHeaders = append(testHeaders, hpack.Header{k, v})
+						}
+					}
+					if !reflect.DeepEqual(testHeaders, Headers) {
+						storyPass = false
+						fmt.Println("False in", f.Name(), "at seq", seq.Seqno)
+						fmt.Println(Headers)
+						fmt.Println(testHeaders)
+						os.Exit(-1)
+						break
 					}
 				}
-				if !reflect.DeepEqual(testHeaders, Headers) {
-					storyPass = false
-					fmt.Println("False in", f.Name(), "at seq", seq.Seqno)
-					fmt.Println(Headers)
-					fmt.Println(testHeaders)
-					os.Exit(-1)
-					break
+				if storyPass {
+					fmt.Println("Pass in", f.Name())
 				}
-			}
-			if storyPass {
-				fmt.Println("Pass in", f.Name())
+			} else if args[1] == "-e" {
+				for _, seq := range jsontype.Cases {
+					Headers := []hpack.Header{}
+					for _, dict := range seq.Headers {
+						for k, v := range dict {
+							Headers = append(Headers, hpack.Header{k, v})
+						}
+					}
+					Wire := hpack.Encode(Headers)
+					if Wire != seq.Wire {
+						storyPass = false
+						fmt.Println("False in", f.Name(), "at seq", seq.Seqno)
+						fmt.Println(Wire)
+						fmt.Println(seq.Wire)
+						os.Exit(-1)
+						break
+					}
+				}
+				if storyPass {
+					fmt.Println("Pass in", f.Name())
+				}
+			} else {
+				fmt.Println("argument should be '-e' or '-d' or none")
+				os.Exit(-1)
 			}
 		}
 	}
